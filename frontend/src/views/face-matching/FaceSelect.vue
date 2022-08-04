d413f3bf7ac8735f5f3ae2d0ff0152af4e0565bd
byunsangun <xdcfvgg@naver.com>
2022-08-04 16:52:37 +0900

    Add selectUser method in FaceSelect.vue

--- frontend/src/views/face-matching/FaceSelect.vue
+++ frontend/src/views/face-matching/FaceSelect.vue
@@ -1,9 +1,27 @@
 <template>
-    <h1>FaceSelect page</h1>
+<div>
+    <h1>8강전 1라운드</h1>
+    <h2>마음이 가는 사용자를 선택해주세요</h2>
+    <img src="../../assets/logo.png" v-on:click="selectUser" />
+    <h1>VS</h1>
+    <img src="../../assets/logo.png" v-on:click="selectUser" />
+</div>
+
 </template>
 
 <script>
 export default {
-    
+    methods: {
+        selectUser() {
+            console.log("haha")
+        }
+    }
 }
 </script>
+
+
+<style scoped>
+img {
+    border: 5px solid rgb(215, 213, 71);
+}
+</style>
